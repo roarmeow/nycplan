cd40e697112efdb361174b663c3cd181ef188a4d
Michael Kranz <michaelkranz@Michaels-MacBook-Pro.local>
2025-11-29 19:44:35 -0500

    Mobile fix for social icons in header 	modified:   dist/css/style.css 	modified:   dist/css/style.css.map 	modified:   frontend/package.json 	modified:   frontend/sass/header.scss 	modified:   functions/enqueue.php 	modified:   style.css

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -1,7 +1,7 @@
 <?php
 function nycp_enqueue() {
-    wp_enqueue_script('nycp_js', get_template_directory_uri() . '/frontend/scripts/app.js', array('jquery'), '1.0');
-    wp_register_style('nycp_css', get_template_directory_uri() . '/dist/css/style.css', false, '1.0');
+    wp_enqueue_script('nycp_js', get_template_directory_uri() . '/frontend/scripts/app.js', array('jquery'), '1.2.1');
+    wp_register_style('nycp_css', get_template_directory_uri() . '/dist/css/style.css', false, '1.2.1');
     wp_enqueue_style('nycp_css');
 }
 add_action( 'wp_enqueue_scripts', 'nycp_enqueue' );
