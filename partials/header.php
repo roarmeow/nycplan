80c1f384ac0825b76007839e44a9117c8694da88
Michael Kranz <michaelkranz@Michaels-MacBook-Pro.local>
2025-11-29 19:00:29 -0500

    Added Bluesky and IG logos/ links to header 	modified:   dist/css/style.css 	modified:   dist/css/style.css.map 	new file:   dist/img/bluesky-logo.svg 	new file:   dist/img/instagram-glyph.svg 	new file:   frontend/img/bluesky-logo.svg 	new file:   frontend/img/instagram-glyph.svg 	new file:   frontend/img/nycplan-logo-mono.svg 	new file:   frontend/img/nycplan-logo.svg 	modified:   frontend/sass/header.scss 	modified:   partials/header.php

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,8 @@
 <?php
-$brand_title = nycp_image_w_class('nycplan-logo.svg');
+$brand_title     = nycp_image_w_class('nycplan-logo.svg');
 $brand_title_srt = '<span class="screen-reader-text">' . get_bloginfo('name') . '</span>';
+$bluesky_logo    = nycp_image_w_class('bluesky-logo.svg');
+$instagram_logo  = nycp_image_w_class('instagram-glyph.svg');
 ?>
 
 <header class='site-header'>
@@ -32,4 +34,22 @@ $brand_title_srt = '<span class="screen-reader-text">' . get_bloginfo('name') . 
     <?php
     endif;
     ?>
+    <ul class="social-links">
+        <li>
+            <a href="https://bsky.app/profile/nycplan.org" title="Follow us on Bluesky" target="_blank">
+                <?php
+                echo $bluesky_logo;
+                ?>
+                <span class="screen-reader-text">Follow us on Bluesky &mdash; opens in a new window</span>
+            </a>
+        </li>
+        <li>
+            <a href="https://www.instagram.com/nyc.plan" title="Follow us on Instagram" target="_blank">
+                <?php
+                echo $instagram_logo;
+                ?>
+                <span class="screen-reader-text">Follow us on Instagram &mdash; opens in a new window</span>
+            </a>
+        </li>
+    </ul>
 </header>
